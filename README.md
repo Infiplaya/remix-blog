6f0cabc77753df0067eb37d0b131a00c8964622e
Pawel Sobaniec <infipablosoban@gmail.com>
2022-10-14 20:41:45 +0200

    add: added simple landing page animation with framer motion

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Personal blog
 
+This was created with big help from https://www.nexxel.dev/blog/remix-blog I mainly just styled it.
 
+Highly recommend checking out more of Nexxel, he is really great guy.
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import { motion } from "framer-motion";
 
 export const meta: MetaFunction = () => {
   return {
@@ -10,31 +11,49 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex justify-center items-center text-center text-4xl px-6 h-screen">
-      <div className="flex flex-col gap-5">
-        <h1 className="font-bold lg:text-6xl">Hello, im <span className="text-indigo-400">Paweł</span>!</h1>
-        <h3 className="text-2xl lg:text-4xl">Welcome to my blog</h3>
-        <Link to={"/blog"}>
-          <button className="pt-6">
-            <span className="font-bold rounded-md text-xl lg:text-3xl bg-indigo-500 px-4 py-2 hover:opacity-90 transition-opacity duration-300 shadow-2xl">
-              START HERE!
-            </span>
-          </button>
-        </Link>
-        <ul className="flex gap-3 mt-10 text-lg self-center">
-          <a href={"https://portfolio-six-gamma-90.vercel.app/"}>
-            <li className="text-indigo-600 hover:text-indigo-700">
-              My Website
-            </li>
-          </a>
-          <a href={"https://github.com/Infiplaya"}>
-            <li className="text-indigo-600 hover:text-indigo-700">Github</li>
-          </a>
-          <a href={"https://www.linkedin.com/in/psobaniec09/"}>
-            <li className="text-indigo-600 hover:text-indigo-700">Linkedin</li>
-          </a>
-        </ul>
+    <motion.div initial="hidden" animate="visible" variants={{
+      hidden: {
+        scale: .8,
+        opacity: 0
+      },
+      visible: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+          delay: .4
+        }
+      },
+    }}>
+      <div className="flex justify-center items-center text-center text-4xl px-6 h-screen">
+        <div className="flex flex-col gap-5">
+          <h1 className="font-bold lg:text-6xl">
+            Hello, im <span className="text-indigo-400">Paweł</span>!
+          </h1>
+          <h3 className="text-2xl lg:text-4xl">Welcome to my blog</h3>
+          <Link to={"/blog"}>
+            <button className="pt-6">
+              <span className="font-bold rounded-md text-xl lg:text-3xl bg-indigo-500 px-4 py-2 hover:opacity-90 transition-opacity duration-300 shadow-2xl">
+                START HERE!
+              </span>
+            </button>
+          </Link>
+          <ul className="flex gap-3 mt-10 text-lg self-center">
+            <a href={"https://psobaniec.vercel.app/"}>
+              <li className="text-indigo-600 hover:text-indigo-700">
+                My Website
+              </li>
+            </a>
+            <a href={"https://github.com/Infiplaya"}>
+              <li className="text-indigo-600 hover:text-indigo-700">Github</li>
+            </a>
+            <a href={"https://www.linkedin.com/in/psobaniec09/"}>
+              <li className="text-indigo-600 hover:text-indigo-700">
+                Linkedin
+              </li>
+            </a>
+          </ul>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
